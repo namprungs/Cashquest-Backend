59666529a0f01fd32ab1df47870f0e1a9b76266c
namprungs <6633168021@student.chula.ac.th>
2025-10-26 20:05:07 +0700

    fix: add test

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { HealthService } from './health.service';
 
 @Module({
   imports: [
@@ -14,6 +15,6 @@ import { AuthModule } from './auth/auth.module';
     AuthModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, HealthService],
 })
 export class AppModule {}
