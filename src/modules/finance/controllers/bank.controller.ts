3d72eeaab2f7d7edfd13a6264d335ead01868767
torrak <156417791+torrak@users.noreply.github.com>
2026-03-04 02:32:20 +0700

    feat: crud bank

---
+++ src/modules/finance/controllers/bank.controller.ts
@@ -0,0 +1,39 @@
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { BankService } from '../services/bank.service';
+import { CreateBankDto } from '../dto/create-bank.dto';
+import { UpdateBankDto } from '../dto/update-bank.dto';
+import { NeededPermissions } from 'src/modules/auth/decorators/needed-permissions.decorator';
+import { PERMISSIONS } from 'src/common/constants/permissions.constant';
+
+@Controller('banks')
+export class BankController {
+  constructor(private readonly bankService: BankService) {}
+
+  @Post()
+  @NeededPermissions([PERMISSIONS.SIMULATION.CONTENT_MANAGE])
+  createBank(@Body() dto: CreateBankDto) {
+    return this.bankService.createBank(dto);
+  }
+
+  @Put(':bankId')
+  @NeededPermissions([PERMISSIONS.SIMULATION.CONTENT_MANAGE])
+  updateBank(
+    @Param('bankId') bankId: string,
+    @Body() dto: UpdateBankDto,
+  ) {
+    return this.bankService.updateBank(bankId, dto);
+  }
+
+  @Get('term/:termId')
+  @NeededPermissions([PERMISSIONS.SIMULATION.CONTENT_MANAGE])
+  listBanksByTerm(@Param('termId') termId: string) {
+    return this.bankService.listBanksByTerm(termId);
+  }
+}
