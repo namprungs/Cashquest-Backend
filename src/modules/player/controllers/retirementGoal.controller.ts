cad0850f5fad69045f6f58fcc967c2070f5b23bd
namprungs <6633168021@student.chula.ac.th>
2026-03-22 18:39:29 +0700

    feat: implement CRUD operations for retirement goals with corresponding DTOs and controller

--- src/modules/player/controllers/retirementGoal.controller.ts
+++ src/modules/player/controllers/retirementGoal.controller.ts
@@ -1,31 +1,72 @@
-import { Controller, Get, Post, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseUUIDPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import type { User } from '@prisma/client';
+import { PERMISSIONS } from 'src/common/constants/permissions.constant';
+import { CurrentUser } from 'src/modules/auth/decorators/current-user.decorator';
+import { NeededPermissions } from 'src/modules/auth/decorators/needed-permissions.decorator';
+import { CreateRetirementGoalDto } from '../dto/create-retirement-goal.dto';
+import { UpdateRetirementGoalDto } from '../dto/update-retirement-goal.dto';
+import { RetirementGoalService } from '../services/retirementGoal.service';
 
-@Controller('retirement-goals')
-export class PlayerController {
-  constructor(private readonly playerController: PlayerController) {}
+@Controller('player/terms/:termId/retirement-goals')
+export class RetirementGoalController {
+  constructor(private readonly retirementGoalService: RetirementGoalService) {}
 
   @Post()
-  create() {
-    return this.playerController.create();
+  @NeededPermissions([PERMISSIONS.SIMULATION.PLAY])
+  create(
+    @Param('termId', new ParseUUIDPipe()) termId: string,
+    @CurrentUser() user: User,
+    @Body() dto: CreateRetirementGoalDto,
+  ) {
+    return this.retirementGoalService.create(termId, user.id, dto);
   }
 
   @Get()
-  findAll() {
-    return this.playerController.findAll();
+  @NeededPermissions([PERMISSIONS.SIMULATION.PLAY])
+  findAll(
+    @Param('termId', new ParseUUIDPipe()) termId: string,
+    @CurrentUser() user: User,
+  ) {
+    return this.retirementGoalService.findAll(termId, user.id);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.playerController.findOne(id);
+  @NeededPermissions([PERMISSIONS.SIMULATION.PLAY])
+  findOne(
+    @Param('termId', new ParseUUIDPipe()) termId: string,
+    @Param('id', new ParseUUIDPipe()) id: string,
+    @CurrentUser() user: User,
+  ) {
+    return this.retirementGoalService.findOne(termId, user.id, id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string) {
-    return this.playerController.update(id);
+  @NeededPermissions([PERMISSIONS.SIMULATION.PLAY])
+  update(
+    @Param('termId', new ParseUUIDPipe()) termId: string,
+    @Param('id', new ParseUUIDPipe()) id: string,
+    @CurrentUser() user: User,
+    @Body() dto: UpdateRetirementGoalDto,
+  ) {
+    return this.retirementGoalService.update(termId, user.id, id, dto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.playerController.remove(id);
+  @NeededPermissions([PERMISSIONS.SIMULATION.PLAY])
+  remove(
+    @Param('termId', new ParseUUIDPipe()) termId: string,
+    @Param('id', new ParseUUIDPipe()) id: string,
+    @CurrentUser() user: User,
+  ) {
+    return this.retirementGoalService.remove(termId, user.id, id);
   }
 }
