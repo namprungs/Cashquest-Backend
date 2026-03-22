be642da1c5fd6ae585b01cad5bb90d2ca2f6f83b
namprungs <6633168021@student.chula.ac.th>
2026-03-22 18:39:29 +0700

    feat: implement CRUD operations for retirement goals with corresponding DTOs and controller

--- src/modules/player/player.module.ts
+++ src/modules/player/player.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
+import { RetirementGoalController } from './controllers/retirementGoal.controller';
 import { PlayerService } from './services/studentProfile.service';
 import { PlayerController } from './controllers/studentProfile.controller';
+import { RetirementGoalService } from './services/retirementGoal.service';
 import { FinanceModule } from '../finance/finance.module';
 
 @Module({
   imports: [FinanceModule],
-  controllers: [PlayerController],
-  providers: [PlayerService],
+  controllers: [PlayerController, RetirementGoalController],
+  providers: [PlayerService, RetirementGoalService],
   exports: [PlayerService],
 })
 export class PlayerModule {}
