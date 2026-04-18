e3e862cb19f8c0b3a031a35c8002a31ce398df9a
namprungs <6633168021@student.chula.ac.th>
2026-04-18 18:44:05 +0700

    feat: implement event listing for investment terms; add filtering options for transactions in wallet service

--- src/modules/investment/investment.module.ts
+++ src/modules/investment/investment.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { InvestmentController } from './controllers/investment.controller';
 import { InvestmentService } from './services/investment.service';
 import { InvestmentCoreService } from './services/investment/investment-core.service';
+import { InvestmentEventsService } from './services/investment/investment-events.service';
 import { InvestmentManagementService } from './services/investment/investment-management.service';
 import { InvestmentMarketService } from './services/investment/investment-market.service';
 import { InvestmentPortfolioService } from './services/investment/investment-portfolio.service';
@@ -11,6 +12,7 @@ import { LivePriceTickScheduler } from './tasks/live-price-tick.scheduler';
   controllers: [InvestmentController],
   providers: [
     InvestmentCoreService,
+    InvestmentEventsService,
     InvestmentMarketService,
     InvestmentManagementService,
     InvestmentPortfolioService,
